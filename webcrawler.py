061c1a2e1d365e183394e2a49834cd899b6fbdb5
QinghaoDraggy <92196671+QinghaoDraggy@users.noreply.github.com>
2023-10-05 16:43:11 -0400

    Added comment upon request

--- webcrawler.py
+++ webcrawler.py
@@ -1,6 +1,10 @@
+### Adding a comment to update per the instructions in the Question 2 of the assignment
+
 from bs4 import BeautifulSoup
 import urllib.request
 #from urllib.request import Request
+
+### Question 1 ###
 
 seed_url = "https://www8.gsb.columbia.edu"
 
@@ -49,3 +53,48 @@ print("num. of URLs seen = %d, and scanned = %d" % (len(seen), len(opened)))
 print("List of seen URLs:")
 for seen_url in seen:
     print(seen_url)
+
+### Question 2 ###
+
+seed_url = "https://www.europarl.europa.eu/news/en/press-room"
+
+urls = [seed_url]
+seen = [seed_url]
+opened = []
+press_releases = []
+
+maxNumUrl = 200  # Increase the maximum as 50 might not be enough to find 10 press releases with "crisis".
+
+while len(urls) > 0 and len(press_releases) < 10 and len(opened) < maxNumUrl:
+    try:
+        curr_url = urls.pop(0)
+        req = urllib.request.Request(curr_url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urllib.request.urlopen(req).read()
+        opened.append(curr_url)
+
+    except Exception as ex:
+        print("Unable to access= " + curr_url)
+        continue
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    # Check if it's a press release related to plenary sessions
+    plenary_tag = soup.find('span', {'class': 'ep_name'}, string='Plenary session')
+    if plenary_tag:
+        content = soup.get_text()
+        if "crisis" in content.lower():  # check if content contains the word 'crisis'
+            press_releases.append(curr_url)
+            save_text_to_file(content, len(press_releases))
+            continue  # Skip adding child urls if this is a press release
+
+    for tag in soup.find_all('a', href=True):
+        childUrl = tag['href']
+        childUrl = urllib.parse.urljoin(seed_url, childUrl)
+        if childUrl not in seen:
+            urls.append(childUrl)
+            seen.append(childUrl)
+
+print("Number of press releases about plenary sessions containing 'crisis':", len(press_releases))
+print("Press releases URLs:")
+for pr_url in press_releases:
+    print(pr_url)
